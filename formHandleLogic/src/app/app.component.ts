349210af58f2a67a951ea920317b169485f9d34d
SHjNEe <baotrung.jpvn@gmail.com>
2023-05-14 18:23:11 +0700

    Add

--- formHandleLogic/src/app/app.component.ts
+++ formHandleLogic/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('form') form: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+  //   onSubmit(form: NgForm) {
+  //     console.log(form);
+  //     console.log(form.value);
+  //   }
+  onSubmit() {
+    console.log(this.form);
+    console.log(this.form.value);
   }
 }
