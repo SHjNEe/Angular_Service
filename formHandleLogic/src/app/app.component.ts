ecf775ff05198d62b36cc91894235268c697ed42
SHjNEe <baotrung.jpvn@gmail.com>
2023-05-16 00:46:24 +0700

    Add

--- formHandleLogic/src/app/app.component.ts
+++ formHandleLogic/src/app/app.component.ts
@@ -6,9 +6,9 @@ import { NgForm } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
   @ViewChild('form') form: NgForm;
+  defaultQuestion = 'pet';
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
